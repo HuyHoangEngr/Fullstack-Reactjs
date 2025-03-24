950c7e284726e73af3c175792d1c70171dfc0c20
HuyHoangEngr <alegiahuy11@gmail.com>
2025-03-24 21:00:08 +0700

    video41.CRUD React 5 - Them moi nguoi dung (CREATE)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
-import { getAllUsers } from "../../services/userService";
+import { getAllUsers, createNewUserService } from "../../services/userService";
 import ModalUser from "./ModalUser";
 
 class UserManage extends Component {
@@ -16,13 +16,17 @@ class UserManage extends Component {
 
     //Goi API
     async componentDidMount() {
+        await this.getAllUsersFromReact();
+    }
+
+    getAllUsersFromReact = async () => {
         let response = await getAllUsers("ALL");
         if (response && response.errCode === 0) {
             this.setState({
                 arrUsers: response.users,
             });
         }
-    }
+    };
 
     handleAddNewUser = () => {
         this.setState({
@@ -35,6 +39,23 @@ class UserManage extends Component {
             isOpenModalUser: !this.state.isOpenModalUser,
         });
     };
+
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUserService(data);
+            if (response && response.errCode !== 0) {
+                alert(response.errMessage);
+            } else {
+                await this.getAllUsersFromReact();
+                this.setState({
+                    isOpenModalUser: false,
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     /**Life cycle
      * Run component:
      * 1. Run construct -> init state
@@ -49,6 +70,7 @@ class UserManage extends Component {
                 <ModalUser
                     isOpen={this.state.isOpenModalUser}
                     toggleFromParent={this.toggleUserModal}
+                    createNewUser={this.createNewUser}
                 />
                 <div className="title text-center">Manage users with Eric</div>
                 <div className="mx-1">
@@ -56,38 +78,39 @@ class UserManage extends Component {
                         className="btn btn-primary px-3"
                         onClick={() => this.handleAddNewUser()}
                     >
-                        <i class="fas fa-plus"></i> Add new user
+                        <i className="fas fa-plus"></i> Add new user
                     </button>
                 </div>
                 <div className="users-table mt-4 mx-1">
                     <table id="customers">
-                        <tr>
-                            <th>Email</th>
-                            <th>First name</th>
-                            <th>Last name</th>
-                            <th>Address</th>
-                            <th>Action</th>
-                        </tr>
-                        {arrUsers &&
-                            arrUsers.map((item, index) => {
-                                console.log("eric check map", item, index);
-                                return (
-                                    <tr key={index}>
-                                        <td>{item.email}</td>
-                                        <td>{item.firstName}</td>
-                                        <td>{item.lastName}</td>
-                                        <td>{item.address}</td>
-                                        <td>
-                                            <button className="btn-edit">
-                                                <i className="fas fa-pencil-alt"></i>
-                                            </button>
-                                            <button className="btn-delete">
-                                                <i className="fas fa-trash"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+                            {arrUsers &&
+                                arrUsers.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className="btn-edit">
+                                                    <i className="fas fa-pencil-alt"></i>
+                                                </button>
+                                                <button className="btn-delete">
+                                                    <i className="fas fa-trash"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
                     </table>
                 </div>
             </div>
