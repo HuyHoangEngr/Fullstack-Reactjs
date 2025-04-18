526744d2411172d23c960b6cc507f024f7e7c416
HuyHoangEngr <alegiahuy11@gmail.com>
2025-04-18 15:55:08 +0700

    video47. Su dung Redux de chuyen doi ngon ngu trong React App

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
 import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils";
+import { changeLanguageApp } from "../../store/actions";
 
 class HomeHeader extends Component {
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+    };
+
     render() {
-        console.log("check language: ", this.props);
+        let language = this.props.lang;
+        console.log("check language: ", language);
         return (
             <React.Fragment>
                 <div className="home-header-container">
@@ -61,8 +68,36 @@ class HomeHeader extends Component {
                                 <i className="fas fa-question-circle"></i>
                                 <FormattedMessage id="homeheader.support" />
                             </div>
-                            <div className="language-vn">VN</div>
-                            <div className="language-en">EN</div>
+                            <div
+                                className={
+                                    language === LANGUAGES.VI
+                                        ? "language-vi active"
+                                        : "language-vi"
+                                }
+                            >
+                                <span
+                                    onClick={() =>
+                                        this.changeLanguage(LANGUAGES.VI)
+                                    }
+                                >
+                                    VN
+                                </span>
+                            </div>
+                            <div
+                                className={
+                                    language === LANGUAGES.EN
+                                        ? "language-en active"
+                                        : "language-en"
+                                }
+                            >
+                                <span
+                                    onClick={() =>
+                                        this.changeLanguage(LANGUAGES.EN)
+                                    }
+                                >
+                                    EN
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -148,7 +183,10 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+    return {
+        changeLanguageAppRedux: (language) =>
+            dispatch(changeLanguageApp(language)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
